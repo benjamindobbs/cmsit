9e51dbfabebb0270363420f02e02522ac463f4bb
benjamindobbs <benjamindobbs@gmail.com>
2021-12-01 16:01:41 -0500

    initial commit

---
+++ pages/Tickets.vue
@@ -0,0 +1,18 @@
+<template>
+<v-app >
+  <v-main class="pt-2 pt-sm-2 pt-xs-2 pt-md-0 pt-lg-0 pt-xl-0" >
+      <v-container fill-height class="ma=0 pa-0">
+        <v-layout>
+        <nuxt/>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSejaFnWhG6ntIhAUpOxhVS3gBPcAidO3lnIEyP61a2IN4hYqA/viewform?embedded=true" frameBorder="0"
+            width="100%"
+            height="100%"
+            class="ma=0 pa-0"
+            >Loading…</iframe
+          >
+        </v-layout>
+      </v-container>
+  </v-main>
+</v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fill-height>
+        <v-layout>
+          <nuxt />
+          <h1>Classical Magnet Info Tech</h1>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
