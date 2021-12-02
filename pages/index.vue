f5e345daba8a28b6b3990c370339e5d9a5b35c78
Dobbs <benjamindobbs@gmail.com>
2021-12-01 20:49:44 -0500

    added drawer functionality and iframe

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,16 @@
         <v-layout>
           <nuxt />
           <h1>Classical Magnet Info Tech</h1>
+          <v-btn
+          to='/newticket'
+          >
+          Submit a New Ticket Here
+          </v-btn>
+          <v-btn
+          to='/ticketqueue'
+          >
+          See the Ticket Queue
+          </v-btn>
         </v-layout>
       </v-container>
     </v-main>
---
+++ pages/ticketqueue.vue
@@ -0,0 +1,18 @@
+<template>
+<v-app >
+  <v-main class="pt-2 pt-sm-2 pt-xs-2 pt-md-0 pt-lg-0 pt-xl-0" >
+      <v-container fill-height class="ma=0 pa-0">
+        <v-layout>
+        <nuxt/>
+          <iframe
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRWiXUpFeLXZumxQY-xUiiQyaCXSAmG20qhgWB5m4ZvX_rUy5gakqtegIMphAxeX0G6xaXrugp6RbYx/pubhtml?gid=1870664925&amp;single=true&amp;widget=true&amp;headers=false" frameBorder="0"
+            width="100%"
+            height="100%"
+            class="ma=0 pa-0"
+            >Loading…</iframe
+          >
+        </v-layout>
+      </v-container>
+  </v-main>
+</v-app>
+</template>
