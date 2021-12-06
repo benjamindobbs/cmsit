94208138b43274d3dae8e30b234aec87aa67f1e0
benjamindobbs <benjamindobbs@gmail.com>
2021-12-06 14:28:23 -0500

    finalization of title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - cmsit',
-    title: 'cmsit',
+    titleTemplate: '%s',
+    title: 'Classical IT',
     htmlAttrs: {
       lang: 'en'
     },
