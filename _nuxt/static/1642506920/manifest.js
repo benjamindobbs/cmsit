fbad0699b7289f95099046ea18331e5aab2b7ebb
benjamindobbs <benjamindobbs@gmail.com>
2022-01-18 06:55:44 -0500

    38bb310

---
+++ _nuxt/static/1642506920/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffaq","\u002Fnewticket","\u002FTutorials","\u002Fticketqueue","\u002Ftutorials"]})
---
+++ _nuxt/static/1642506920/ticketqueue/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/ticketqueue", {data:[{}],fetch:{},mutations:void 0});
